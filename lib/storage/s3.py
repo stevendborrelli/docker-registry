2b5714bb6c605980b343d58b9500c3a20c2338f7
Steven Borrelli <steve@borrelli.org>
2013-12-04 09:18:52 -0600

    Add ability to set host for s3 lib

--- lib/storage/s3.py
+++ lib/storage/s3.py
@@ -18,6 +18,7 @@ class S3Storage(Storage):
         self._s3_conn = \
             boto.s3.connection.S3Connection(self._config.s3_access_key,
                                             self._config.s3_secret_key,
+                                            host=self._config.s3_host,
                                             is_secure=False)
         self._s3_bucket = self._s3_conn.get_bucket(self._config.s3_bucket)
         self._root_path = self._config.storage_path
